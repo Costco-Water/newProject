58a687fafe621f1bd915af7f58bb5a977cfc4389
Costco Water <turnbull1995@gmail.com>
2025-02-14 11:12:28 +0000

    Division Function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def Addition(a, b):
 Addition(10,5)
 
 
-def Subtraction (a, b):
+def Subtraction(a, b):
     print(f"{a - b}")
 Subtraction(10,5)
+
+def Division(a, b):
+    print(f"{a / b}")
+Division(10,2)
